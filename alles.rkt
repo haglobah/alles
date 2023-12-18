ecc6a1010c7e29be412e5e579b448e12305f95af
Beat Hagenlocher <bah@posteo.de>
2023-12-18 17:00:56 +0100

    Get keyboard input to work in racket/gui

--- alles.rkt
+++ alles.rkt
@@ -1,28 +1,54 @@
-#lang racket
+#lang racket/base
 
-(require racket/gui/easy
-         racket/class
-         racket/draw)
+(require racket/gui)
 
-(define (run prog . args)
-  (apply system* (find-program prog) args))
+; (define frame (new frame% [style '(float)] [label "Example"]))
+
+
+; ; Make a button in the frame
+; (new button% [parent frame]
+;              [label "Click Me"]
+;              ; Callback procedure for a button click:
+;              [callback (lambda (button event)
+;                          (send msg set-label "Button click"))])
+
+; (send frame show #t)
+
+; (new canvas% [parent frame]
+;              [paint-callback
+;               (lambda (canvas dc)
+;                 (send dc set-scale 3 3)
+;                 (send dc set-text-foreground "blue")
+;                 (send dc draw-text "Don't Panic!" 0 0))])
+
+
+(define frame (new frame%
+                   [label "Example"]
+                   [x 300]
+                   [y 300]
+                   [width 300]
+                   [height 300]
+                   [style '(float)]))
+
+(define msg (new message% [parent frame]
+                          [label "Nothing happened so far."]))
+
+(define (get-key keycode)
+  (cond
+    [(symbol? keycode) (symbol->string keycode)]
+    [(char? keycode) (string keycode)]
+    [else "how?"]))
+
+(define my-canvas%
+  (class canvas% ; The base class is canvas%
+    ; Define overriding method to handle keyboard events
+    (define/override (on-char key-event)
+      (define keypress (get-key (send key-event get-key-code)))
+      (send msg set-label keypress))
+    ; Call the superclass init, passing on all init args
+    (super-new)))
  
-(define (find-program str)
-  (or (find-executable-path str)
-      (error 'pfsh "could not find program: ~a" str)))
+; Make a canvas that handles events in the frame
+(new my-canvas% [parent frame])
 
-(render
- (window
-  #:title "alles"
-  #:size '(400 #f)
-  #:position 'center
-  #:style '(no-resize-border)
-  (vpanel
-    (input "lala"
-         #:enabled? #t
-         #:min-size '(#f 75)
-         #:font (make-object font%
-                  48
-                  'modern))
-    (button "exec-things"
-      (λ () (system "ydotool mousemove 100 100"))))))
+(send frame show #t)
